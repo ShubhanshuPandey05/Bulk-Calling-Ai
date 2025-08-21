0838cd87272a9fbab77a85a9d6b9156332ea3a79
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2025-08-21 16:41:25 +0530

    changed the port

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ app.post('/response', (req, res) => {
     res.status(201).json({"message":"Added the user response"})
 })
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log("Server is running on port 3000");
 });
 
